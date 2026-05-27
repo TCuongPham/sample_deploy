2455b44b20019520b15d8b624653e4b9a46f3b2d
SeaAsteroid <phamtrieucuong2005@gmail.com>
2026-05-27 21:33:43 +0700

    Fix CORS policy

--- backend/server.js
+++ backend/server.js
@@ -37,8 +37,10 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 // Start server
-app.listen(3001, () => {
-  console.log("Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 app.get("/api/users", async (req, res) => {
   try {
